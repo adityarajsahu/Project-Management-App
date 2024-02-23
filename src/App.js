3bb92d542e4fb4152b007cb0baba51ed7ba495c8
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2024-02-23 23:21:23 +0530

    styled sidebar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+import ProjectsSidebar from "./components/ProjectsSidebar";
+
 function App() {
     return (
-        <React.Fragment>
-            <h1 className="my-8 text-center text-5xl font-bold">Hello World!</h1>
-        </React.Fragment>
+        <main className="h-screen my-8">
+            <ProjectsSidebar />
+        </main>
     );
 }
 
